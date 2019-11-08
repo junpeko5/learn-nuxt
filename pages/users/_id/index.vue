d4278e6afc9a452ac30d97a892db720ed1ca63ce
junpeko5 <junpeko5@gmail.com>
2019-11-08 20:40:03 +0900

    Nuxt validate component

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -3,3 +3,11 @@
     <h2>This is user's id {{ $route.params.id }}</h2>
   </div>
 </template>
+
+<script>
+export default {
+  validate({params}) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
