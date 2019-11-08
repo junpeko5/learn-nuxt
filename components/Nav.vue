9d9793df0ccc8ec446ab771b89a824ce420b4dc5
junpeko5 <junpeko5@gmail.com>
2019-11-09 07:11:07 +0900

    Usng nuxt link

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Nuxt App</a>
+    <nuxt-link to="/" class="navbar-brand">Nuxt App</nuxt-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -8,10 +8,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="/">Home</a>
+          <nuxt-link to="/" class="nav-link">Home</nuxt-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/users">Users</a>
+          <nuxt-link to="/users" class="nav-link">Users</nuxt-link>
         </li>
       </ul>
     </div>
