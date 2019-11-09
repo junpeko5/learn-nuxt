eb54af3f9283cd219845391ef16b16e3e8e33b4b
junpeko5 <junpeko5@gmail.com>
2019-11-09 09:02:15 +0900

    Vue Component

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,17 +2,21 @@
   <div>
     <h2>Making API request - the Vue way</h2>
     <div class="container">
-      <h4 v-for="post in posts" :key="post.id">
-        {{ post.title }}
-      </h4>
+      <div class="row">
+        <Card v-for="post in posts" :key="post.id" :post="post" class="ml-auto mr-auto" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Card from '@/components/Card'
 
 export default {
+  components: {
+    Card
+  },
   data() {
     return {
       posts: ''
